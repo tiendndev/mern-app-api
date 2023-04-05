ad2f667074824a7da29029d2b65d0903d1c56eb9
tiendndev <birongze300401@gmail.com>
2023-04-05 15:11:49 +0700

    Drag & Drop column and card

--- src/validations/card.validation.js
+++ src/validations/card.validation.js
@@ -5,7 +5,7 @@ const createNew = async (req, res, next) => {
    const condition = Joi.object({
       boardId: Joi.string().required(),
       columnId: Joi.string().required(),
-      title: Joi.string().required().min(3).max(30).trim(),
+      title: Joi.string().required().min(3).max(100).trim(),
    });
    try {
       await condition.validateAsync(req.body, { abortEarly: false });
@@ -17,4 +17,23 @@ const createNew = async (req, res, next) => {
    }
 };
 
-export const CardValidation = { createNew };
+const update = async (req, res, next) => {
+   const condition = Joi.object({
+      title: Joi.string().min(3).max(100).trim(),
+      boardId: Joi.string(),
+      columnId: Joi.string(),
+   });
+   try {
+      await condition.validateAsync(req.body, {
+         abortEarly: false,
+         allowUnknown: true,
+      });
+      next();
+   } catch (error) {
+      res.status(HttpStatusCode.BAD_REQUEST).json({
+         errors: new Error(error).message,
+      });
+   }
+};
+
+export const CardValidation = { createNew, update };
